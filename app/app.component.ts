3676bea90d08f400c222fa8185d533239dc753ac
Aniket Deshbhratar <aniket@madasolns.com>
2020-04-16 23:09:37 +0530

    till data binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector:"pm-root",
+  template:`
+    <div>
+      <h1>{{pageTitle}}</h1>
+      <pm-products></pm-products>
+    </div>
+
+  `
+
 })
 export class AppComponent {
-  title = 'gs';
+  pageTitle:string = 'Acme Product management'
 }
