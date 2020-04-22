b6ee6c6ada8c52935d30721bd170491d3d08d67f
Aniket Deshbhratar <aniket@madasolns.com>
2020-04-22 23:13:14 +0530

    all about routes

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,11 +4,32 @@ import { Component } from '@angular/core';
 @Component({
   selector:"pm-root",
   template:`
-    <div>
-      <h1>{{pageTitle}}</h1>
-      <pm-products></pm-products>
+  <header>
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="#">{{pageTitle}}</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" [routerLink]="['/welcome']">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" [routerLink]="['/products']">Products</a>
+            </li>
+          
+          </ul>
+          <form class="form-inline mt-2 mt-md-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+    </header>
+    <div class="container">
+    <router-outlet></router-outlet>
     </div>
-
   `
 
 })
